65516eae821a0f68a5ae70e3f8e957eb84ee94bc
vedicmonk <vedicmonk@win>
2025-04-27 13:40:58 +0200

    rc try

--- rust/microcopier/src/main.rs
+++ rust/microcopier/src/main.rs
@@ -1,12 +1,24 @@
-use std::{ collections::HashMap, env::{ args, current_dir }, error::Error, fs, path::PathBuf };
+use std::{
+    collections::HashMap,
+    env::{ args, current_dir },
+    error::Error,
+    fs,
+    path::PathBuf,
+    rc::Rc,
+};
 
-fn main() /*-> Result<(), Box<dyn Error>>*/ {
-    let args: Vec<String> = args().collect();
+fn main() {
+    /*-> Result<(), Box<dyn Error>>*/ let args: Vec<String> = args().collect();
 
     let src = get_abs(&args[1]).unwrap();
     let dest = get_abs(&args[2]).unwrap();
+    let dest_components: Vec<String> = dest
+        .components()
+        .map(|item| item.as_os_str().to_string_lossy().into_owned())
+        .collect();
+    let dest_rc = Rc::new(dest_components);
 
-    let file_map = read_dir(&src, &dest).unwrap();
+    let file_map = read_dir(&src, dest_rc.clone()).unwrap();
 
     file_map.iter().for_each(|entry| {
         println!("{:?} -> {:?}", *entry.0, *entry.1);
@@ -21,8 +33,7 @@ fn get_abs(path: &String) -> Result<PathBuf, Box<dyn Error>> {
     let abs_path = if !p.is_absolute() {
         let mut cur_dir = current_dir()?;
         cur_dir.push(p);
-        cur_dir=PathBuf::from(cur_dir.canonicalize()?);
-        println!("{:?}", cur_dir);
+        cur_dir = PathBuf::from(cur_dir.canonicalize().unwrap());
         cur_dir
     } else {
         p
@@ -33,32 +44,27 @@ fn get_abs(path: &String) -> Result<PathBuf, Box<dyn Error>> {
 
 fn read_dir(
     src_root: &PathBuf,
-    dest_root: &PathBuf
-) -> Result<HashMap<PathBuf, PathBuf>, Box<dyn Error>> {
-    let mut file_map: HashMap<PathBuf, PathBuf> = HashMap::new();
+    dest_root: Rc<Vec<String>>
+) -> Result<HashMap<PathBuf, Rc<Vec<String>>>, Box<dyn Error>> {
+    let mut file_map: HashMap<PathBuf, Rc<Vec<String>>> = HashMap::new();
 
     match fs::read_dir(src_root) {
         Err(e) => eprintln!("Could not find directory: {}", e),
         Ok(entries) => {
             for entry in entries {
-                let src_child = entry?.path();
+                let src_child = entry.unwrap().path();
                 let leaf_name = src_child.file_name().unwrap().to_str().unwrap().to_string();
+                let dest_child = dest_root;
+                dest_child.push(leaf_name);
 
                 if src_child.is_dir() {
-                    let mut dest_child = dest_root.clone();
-                    dest_child.push(leaf_name);
-                    let child_file_map = read_dir(&src_child, &dest_child)?;
+                    let child_file_map = read_dir(&src_child, dest_child).unwrap();
                     child_file_map.iter().for_each(|child| {
-                        file_map.insert(
-                            child.0.canonicalize().unwrap(),
-                            child.1.canonicalize().unwrap()
-                        );
+                        file_map.insert(child.0.canonicalize().unwrap(), *child.1);
                     });
                 } else {
-                    file_map.insert(
-                        src_root.join(&leaf_name).canonicalize()?,
-                        dest_root.join(&leaf_name).canonicalize()?
-                    );
+                    println!("{:?}", file_map);
+                    file_map.insert(src_root.join(&leaf_name).canonicalize().unwrap(), dest_child);
                 }
             }
         }
