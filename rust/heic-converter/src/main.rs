61c36aa41e9c855566d57795364628bc9eee7be1
vedicmonk <vedicmonk@win>
2025-07-13 15:43:55 +0200

    heic converter rust

--- rust/heic-converter/src/main.rs
+++ rust/heic-converter/src/main.rs
@@ -17,22 +17,61 @@ use libheif_rs::{
 };
 
 fn main() -> Result<()> {
-    let img = ImageReader::open("input.png").unwrap().decode().unwrap().;
+    let img = ImageReader::open("input.png").unwrap().decode().unwrap();
     let rgba = img.to_rgb8();
     let (h,w) = rgba.dimensions();
 
+    let mut image = Image::new(
+        w,
+        h,
+        ColorSpace::Rgb(RgbChroma::C444)
+    )?;
+
+    image.create_plane(Channel::R, w, h, 8)?;
+    image.create_plane(Channel::G, w, h, 8)?;
+    image.create_plane(Channel::B, w, h, 8)?;
+
+    let planes = image.planes_mut();
+    let plane_r = planes.r.unwrap();
+    let stride = plane_r.stride;
+
+    let data_r = plane_r.data;
+    let data_g = planes.g.unwrap().data;
+    let data_b = planes.b.unwrap().data;
+
+    for y in 0..h {
+        let mut pixel_index = stride * y as usize;
+        for x in 0..w {
+            let color = ((x * y) as u32).to_le_bytes();
+            data_r[pixel_index] = color[0];
+            data_g[pixel_index] = color[1];
+            data_b[pixel_index] = color[2];
+            pixel_index += 1;
+        }
+    }
+
     let lib_heif = LibHeif::new();
-    let mut encoder = lib_heif.encoder_for_format(CompressionFormat::Hevc)?;
+    let mut encoder = lib_heif.encoder_for_format(CompressionFormat::Hevc).unwrap();
     encoder.set_quality(EncoderQuality::Lossy(90))?;
+    let mut context = HeifContext::new()?;
+    context.encode_image(&image, &mut encoder, None)?;
+
+    // OpenOptions::new()
+    //     .create_new(true)
+    //     .write(true)
+    //     .append(true)
+    //     .open("output.heic")
+    //     .unwrap();
+    File::create_new("output.heic").unwrap();
+    context.write_to_file("output.heic")?;
 
     //let mut ctx = HeifContext::read_from_file("input.png")?;
-    let mut ctx = HeifContext::new().unwrap();
+    // let mut ctx = HeifContext::new().unwrap();
     //ctx.encode_image(&img, &mut encoder, None);
 
-    let output_path = "output.heic";
-    let mut heif_img = 
+    // let output_path = "output.heic";
 
-    ctx.write_to_file(output_path).unwrap();
+    // ctx.write_to_file(output_path).unwrap();
 
     //let mut heif_img = Image::new(h, w, ColorSpace::Rgb(rgba))?;
 
